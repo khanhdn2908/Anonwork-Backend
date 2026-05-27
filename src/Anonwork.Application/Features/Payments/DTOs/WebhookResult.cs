515a5f358e810fec59f1adeef9648f858d71c972
khanh <khanhdn.business@gmail.com>
2026-05-27 17:46:53 +0700

    fix success respone payment

--- src/Anonwork.Application/Features/Payments/DTOs/WebhookResult.cs
+++ src/Anonwork.Application/Features/Payments/DTOs/WebhookResult.cs
@@ -13,12 +13,7 @@ namespace Anonwork.Application.Features.Payments.DTOs
 
         private WebhookResult() { }
 
-        public static WebhookResult Ok(string v) => new() { Success = true };
+        public static WebhookResult Ok(string? v = null) => new() { Success = true };
         public static WebhookResult Fail(string message) => new() { Success = false, ErrorMessage = message };
-
-        internal static WebhookResult Ok()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
